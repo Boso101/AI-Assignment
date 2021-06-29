715548b5a762a6bdde54ae150654762219a1495a
Boso101 <bosorevived@gmail.com>
2021-06-29 11:14:32 +0930

    Grid Creation
    Grid Creation

--- 3 - Implementation/Unity/AI-Implementation/Assets/Resources/Scripts/Game/Game.cs
+++ 3 - Implementation/Unity/AI-Implementation/Assets/Resources/Scripts/Game/Game.cs
@@ -4,12 +4,47 @@ using UnityEngine;
 
 public class Game : MonoBehaviour
 {
+    [Header("Settings")]
+    public int rows;
+    public int columns;
 
-   public void CreateGrid(int rows, int columns)
+    [SerializeField] protected GameObject gridPrefab;
+    /// <summary>
+    /// Here we will use some sort of Node system to create our grids for our AI to then use 
+    /// </summary>
+    /// <param name="rows"></param>
+    /// <param name="columns"></param>
+    /// 
+
+
+    private void Start()
     {
+        CreateGrid(rows, columns);
+    }
+
+    public void CreateGrid(int rows, int columns)
+    {
+
+        for (int i = 0; i < rows; i++)
+        {
+
+
+            for (int j = 0; j < columns; j++)
+            {
+                //Instantiate Grid Object 
+                GameObject gridObj = Instantiate(gridPrefab);
+                gridObj.transform.position = new Vector2(i, j);
+
+
+            }
+
+        }
 
     }
 
+
+
+    
     public void PlaceFood(Vector2 pos)
     {
 
