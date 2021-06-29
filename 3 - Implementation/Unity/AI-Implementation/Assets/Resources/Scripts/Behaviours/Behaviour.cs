b1420ea8d8a943bd4fe733b91f7d72aa3055fb1f
Boso101 <bosorevived@gmail.com>
2021-06-29 10:50:52 +0930

    Behaviour class

---
+++ 3 - Implementation/Unity/AI-Implementation/Assets/Resources/Scripts/Behaviours/Behaviour.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public abstract class Behaviour
+{
+    public Behaviour()
+    {
+
+    }
+
+    public abstract bool Update(Agent agent, float deltaTime);
+ 
+}
