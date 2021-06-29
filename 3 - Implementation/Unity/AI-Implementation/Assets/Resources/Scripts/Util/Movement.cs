a71a96aaa6f9d9e6b3b6aff5066c9596550f43f6
Boso101 <bosorevived@gmail.com>
2021-06-29 11:02:54 +0930

    Movement Class

---
+++ 3 - Implementation/Unity/AI-Implementation/Assets/Resources/Scripts/Util/Movement.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Movement : MonoBehaviour
+{
+    protected Vector2 velocity;
+    protected Vector2 force;
+
+
+
+
+
+    public Vector2 GetVelocity()
+    {
+        return velocity;
+    }
+
+    public void SetVelocity(Vector2 vel)
+    {
+        velocity = vel;
+    }
+
+    public Vector2 GetForce()
+    {
+        return force;
+    }
+
+    public void SetForce(Vector2 force)
+    {
+        this.force = force;
+    }
+}
