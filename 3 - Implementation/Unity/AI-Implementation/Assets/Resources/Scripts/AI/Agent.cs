168a5dcac6ac9723a4860332c44c2441eb9c8d85
Boso101 <bosorevived@gmail.com>
2021-07-12 15:06:49 +0930

    Agents made to fit new system

--- 3 - Implementation/Unity/AI-Implementation/Assets/Resources/Scripts/AI/Agent.cs
+++ 3 - Implementation/Unity/AI-Implementation/Assets/Resources/Scripts/AI/Agent.cs
@@ -4,5 +4,36 @@ using UnityEngine;
 
 public class Agent : MonoBehaviour
 {
-   public static Vector2 playerPosition;
+    public static Vector2 playerPosition;
+    public static Astar pathFinding;
+    protected Vector2 targetLocation;
+
+    // How many units do this move per move update
+    [SerializeField] protected int unitsPerMove = 1;
+
+    //path container
+    protected PathContainer container;
+    
+
+    public void Move(Vector2 newPos)
+    {
+        transform.position = newPos;
+    }
+
+    private Vector2 CalculateMovement()
+    {
+        return Vector2.zero;
+    }
+
+    private void Start()
+    {
+        container = new PathContainer();
+
+        //TODO: Maybe just dont have an instance of pathfinding for each agent.
+        if(pathFinding == null) pathFinding = new Astar();
+    }
+    private void Update()
+    {
+        
+    }
 }
