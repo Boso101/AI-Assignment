4a21cea0eef5013bc627c5ec82e9a93edb2dd48b
Boso101 <bosorevived@gmail.com>
2021-07-19 15:25:12 +0930

    Update EditorScript.cs

--- 3 - Implementation/Unity/AI-Implementation/Assets/Resources/Scripts/EditorScripts/EditorScript.cs
+++ 3 - Implementation/Unity/AI-Implementation/Assets/Resources/Scripts/EditorScripts/EditorScript.cs
@@ -8,15 +8,27 @@ public class EditorScript : MonoBehaviour
     protected Agent playerObject;
 
     public Text playerMoveSpeed;
-    public Text playerMovementCost;
-    public Text diagonalmovementCost;
+    public Text playerStraightCost;
+    public Text diagonalMovementCost;
 
-
+    private void Start()
+    {
+        playerObject = GameObject.FindGameObjectWithTag("Player").GetComponent<Agent>();
+    }
 
     public void Apply()
     {
         playerObject.MoveTime = int.Parse(playerMoveSpeed.text);
-        playerObject.movem = int.Parse(playerMoveSpeed.text);
+        playerObject.StraightCost = int.Parse(playerStraightCost.text);
+        playerObject.DiagonalCost = int.Parse(diagonalMovementCost.text);
 
+
+    }
+
+    public void UpdateEntityInfo()
+    {
+        playerMoveSpeed.text = playerObject.MoveTime.ToString();
+        playerStraightCost.text = playerObject.StraightCost.ToString();
+        diagonalMovementCost.text = playerObject.DiagonalCost.ToString();
     }
 }
