484a1843e32a30b35d3952d12116c8a3b0532b6e
Boso101 <bosorevived@gmail.com>
2021-07-20 15:29:42 +0930

    Update SpeedBoost.cs

--- 3 - Implementation/Unity/AI-Implementation/Assets/Resources/Scripts/PowerUp/SpeedBoost.cs
+++ 3 - Implementation/Unity/AI-Implementation/Assets/Resources/Scripts/PowerUp/SpeedBoost.cs
@@ -12,6 +12,10 @@ public class SpeedBoost : PowerUp
         {
             agent.MoveTime = 0.15f;
         }
-        Destroy(gameObject);
+        else
+        {
+            Destroy(gameObject);
+
+        }
     }
 }
